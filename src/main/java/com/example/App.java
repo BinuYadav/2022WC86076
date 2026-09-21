7e17684fdc09fc343931a446605b92c906b740ba
Binu Yadav <binuyadav296@email.com>
2026-09-21 21:41:00 +0530

    Initial Maven project for DevOps lab

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("BITS DevOps Lab");
+        System.out.println("BITS ID: 2022WC86076");
+        System.out.println("Jenkins Continuous Integration Test");
+
+    }
+}
